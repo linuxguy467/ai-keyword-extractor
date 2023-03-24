fec291c7da88cb6fbf3b6cf111a95f27a44a436c
linuxguy467 <me@matthem.codes>
2023-03-24 08:08:09 -0400

    add openai extract keywords logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react'
 import { Box, Container } from '@chakra-ui/react'
 import { Footer, Header, TextInput } from './components'
 
 const App = () => {
-  const extractKeywords = (text) => {
-    console.log(text)
+  const [keywords, setKeywords] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const extractKeywords = async (text) => {
+    setLoading(true)
+    setIsOpen(true)
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and seperate with commas\n\n' + text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8,
+      }),
+    }
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options)
+
+    const json = await response.json()
+
+    const data = json.choices[0].text.trim()
+
+    console.log(data)
   }
 
   return (
